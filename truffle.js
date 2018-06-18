61779ac8b8b542657716d6f642f3630cdb3332c4
jhamPac <cloud@kevia.me>
2018-06-17 20:51:06 -0700

    set truffle network config correctly;

--- truffle.js
+++ truffle.js
@@ -1,7 +1,9 @@
 module.exports = {
   networks: {
-    host: "127.0.0.1",
-    port: 7545,
-    network_id: "*"
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // match any network
+    }
   }
 };
