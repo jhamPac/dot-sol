5f7baad8f73fc685cd454ac2d639f1a6b898e89a
jhamPac <cloud@kevia.me>
2018-07-01 15:27:02 -0700

    finished writing tests;

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -6,4 +6,12 @@ import "../contracts/Adoption.sol";
 
 contract TestAdoption {
   Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+  function testUserCanAdoptPet() public {
+    uint returnedId = adoption.adopt(8);
+
+    uint expected = 8;
+
+    Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded");
+  }
 }
