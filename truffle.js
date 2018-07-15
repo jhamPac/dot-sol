7a161f349a4f48b543d71d5f0b214c5a3184593c
jhamPac <cloud@kevia.me>
2018-07-15 16:14:10 -0700

    testing finally worked;

--- truffle.js
+++ truffle.js
@@ -6,5 +6,11 @@ module.exports = {
       network_id: "*",
       from: "0x8D06068e87b4aC0982581557878Dd7b0d54010BD"
     }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD'
+    }
   }
 };
