67fcbfc77f338e6584b175dd282b1b575894c5b7
jhamPac <cloud@kevia.me>
2018-08-16 09:21:41 -0700

    removed default from so I can use ganache;

--- truffle.js
+++ truffle.js
@@ -3,8 +3,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*",
-      from: "0x8D06068e87b4aC0982581557878Dd7b0d54010BD"
+      network_id: "*"
     }
   },
   mocha: {
